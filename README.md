6fc9f975dc6eab147d3f59aa3cfa491c163c026b
Shubham Singh <shubhamsinghshubham777@gmail.com>
2025-12-28 01:39:42 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# playtogether
+
+A simple shared media player for all your favorite platforms (powered by Flutter).
---
+++ android/app/src/main/kotlin/app/playtogether/playtogether/MainActivity.kt
@@ -0,0 +1,5 @@
+package app.playtogether.playtogether
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
